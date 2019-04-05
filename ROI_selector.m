f0ad369b39395f48b03c3fe9a5817648b8378db7
Arya Massarat <aryam7@gmail.com>
2019-04-05 14:48:03 -0700

    improve ROI_selector so that it doesn't simply save files to the root dir

--- ROI_selector.m
+++ ROI_selector.m
@@ -1,3 +1,9 @@
-[filename, pathname]=uiputfile('*.txt','Save');
+[file, path]=uiputfile('*.txt','Save ROI Labels', 'roi_labels.txt');
 vname=@(x) inputname(1);
-writetable(ROI,filename,'Delimiter','\t');
+if isequal(file,0) || isequal(path,0)
+   disp('User clicked Cancel.')
+else
+   disp(['User selected ',fullfile(path,file),...
+         ' and then clicked Save.'])
+writetable(ROI,fullfile(path,file),'Delimiter','\t');
+end
