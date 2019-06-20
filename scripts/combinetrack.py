cf0fd45a4aaf4e20b7c4e236fead7cc0d416fbe9
JarredAllen <jarredallen73@gmail.com>
2019-06-20 16:57:16 -0700

    Added a file to combine the tracks from each ROI back into one file after being split

---
+++ scripts/combinetrack.py
@@ -0,0 +1,39 @@
+import argparse
+
+def combine_split_track(tracks, outfile, filesep='SPLIT #{number}:\n'):
+    """Combines a list of tracks (specified via filenames) into one
+    file, at the location specified by outfile.
+
+    filesep is used as a delimiter to separate the different files.
+    Valid format arguments to be used in it are:
+      · number - The number corresponding to this split
+    """
+    f = open(outfile, 'w')
+    for infile in tracks:
+        f.write(filesep.format(number=infile.split('/')[-2]))
+        f.write(open(infile).read().strip()+'\n')
+    f.close()
+
+def main():
+    args = argparse.ArgumentParser()
+    args.add_argument('outfile',
+                      type=str,
+                      help='The file to which to write the combined output.',
+                     )
+    args.add_argument('infiles',
+                      type=str,
+                      nargs='+',
+                      help='A list of all the files which are to be combined.',
+                     )
+    args.add_argument('--filesep',
+                      type=str,
+                      dest='filesep',
+                      default='SPLIT #{number}:\n',
+                      help='A string to preface each file being merged.',
+                     )
+    args = args.parse_args()
+    combine_split_track(args.infiles, args.outfile, filesep=args.filesep)
+
+if __name__ == '__main__':
+    main()
+
