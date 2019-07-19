cbee568dba5b8afbdd18e92e8f1bb0e6550f4b9e
JarredAllen <jarredallen73@gmail.com>
2019-07-19 13:29:11 -0700

    Updated roidefine to allow appending

--- scripts/roidefine.py
+++ scripts/roidefine.py
@@ -6,18 +6,27 @@ import os, os.path
 
 import bbox
 
-def roi_poly_input(video, show_image=False):
+def roi_poly_input(video, show_image=True, old_bboxes=[]):
     if not os.path.isfile(video):
         raise RuntimeError(f'{video} is not a file.')
     if show_image:
+        print('Loading image')
         ret, img = cv2.VideoCapture(video).read()
         if not ret:
             raise RuntimeError(f'Unknown error reading {video}')
+        print('Image loaded')
         plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        for box in old_bboxes:
+            xs = [pt[0] for pt in box.poly_abspos]
+            xs.append(xs[0])
+            ys = [pt[1] for pt in box.poly_abspos]
+            ys.append(ys[0])
+            # plt.add_line(plt.Line2D(xs, ys))
+            plt.plot(xs, ys, 'o-')
         try:
             from roipoly import MultiRoi
         except ImportError:
-            print('Module roipoly is not installed, using backup method.')
+            print('Module roipoly is not present, using backup method.')
             plt.draw()
         else:
             # Only run this code if roipoly can be imported
@@ -60,7 +69,11 @@ def main():
                             help='Do not input polygons, only rectangular '
                                  'bboxes.')
     args = arg_parser.parse_args()
-    rois = roi_poly_input(args.infile)
+    if os.path.isfile(args.outfile):
+        rois = bbox.read_bboxes(args.outfile)
+    else:
+        rois = []
+    rois += roi_poly_input(args.infile, old_bboxes=rois)
     bbox.save_rois(rois, args.outfile)
 
 if __name__ == '__main__':
