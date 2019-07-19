fec89ce4cd6b09b2ae1ea884b1f2ac3c52541e66
JarredAllen <jarredallen73@gmail.com>
2019-07-19 16:39:07 -0700

    Updated edgefromtrack to only look at important edges, haven't tested

--- scripts/edgefromtrack.py
+++ scripts/edgefromtrack.py
@@ -6,21 +6,51 @@ import numpy as np
 
 import bbox
 
-def find_floor_angle(angles, target_angle):
-    """Takes a list of angles and returns the angle in that list which
-    is closest counterclockwise of the given angle, as a (index, value)
-    tuple.
+class Interval:
+    """A class for storing an interval of angles."""
+    def __init__(self, floor, ceil, payload=None):
+        """Create a new interval with lower bound floor and upper bound
+        ceil.
 
-    This is used to find which edge is crossed in the convert function.
+        If specified, payload is just extra optional data that it can
+        hold. I use it to store the edge number.
+        """
+        self.floor = floor
+        self.ceil = ceil
+        self.payload = payload
+
+    def contains(self, value):
+        """Returns true iff value is between self.floor and self.ceil"""
+        if self.floor < self.ceil:
+            return self.floor < value and self.ceil > value
+        else:
+            return self.floor > value and self.ceil < value
+
+    def distance(self, value, modulus=2*math.pi):
+        """Returns the distance from value to this interval, under the
+        given modulus.
+        """
+        if self.contains(value):
+            return 0
+        else:
+            floordist = self.floor - value
+            if floordist < 0:
+                floordist += modulus
+            ceildist = value - self.ceil
+            if ceildist < 0:
+                ceildist += modulus
+            return min(floordist, ceildist)
+
+def closest_interval(value, intervals, modulus=2*math.pi):
+    """Given a value, a list of intervals, and a modulus, it returns
+    the closest interval in the list to the given value.
     """
-    curr = -1, 100
-    for index, angle in enumerate(angles[1:], 1):
-        if angle < curr[1] and angle >= target_angle:
-            curr = index, angle
-    if curr[0] == -1:
-        m = min(angles)
-        return angles.index(m), m
-    return curr
+    closest = (None, modulus*2)
+    for interval in intervals:
+        distance = interval.distance(value, modulus)
+        if distance < closest[1]:
+            closest = interval, distance
+    return closest
 
 def convert(infile, outfile, bboxes):
     """Loads infile and converts it to which edges were crossed, as
@@ -36,6 +66,16 @@ def convert(infile, outfile, bboxes):
                for i in range(len(verts))]
     angles = [[round((math.atan2(vert[1], vert[0])-offsets[i]) % (2*math.pi), 3)
                for vert in verts[i]] for i in range(len(verts))]
+    intervals = []
+    for i, box in bboxes:
+        box_intervals = []
+        for edge in bbox.edgs:
+            box_intervals.append(Interval(
+                angles[i][edge],
+                angles[i][(edge+1) % len(angles[i])],
+                edge
+            ))
+        intervals.append(box_intervals)
     inp = open(infile)
     outp = open(outfile, 'w')
     outp.write('roi,id,edge0,t0,edge1,t1,number_warning\n')
@@ -46,11 +86,11 @@ def convert(infile, outfile, bboxes):
         x0 -= centers[roinum][0]
         y0 -= centers[roinum][1]
         a0 = (math.atan2(y0, x0) - offsets[roinum]) % (2*math.pi)
-        e0 = find_floor_angle(angles[roinum], a0)[0]
+        e0 = closest_interval(a0, intervals[roinum])
         x1 -= centers[roinum][0]
         y1 -= centers[roinum][1]
         a1 = (math.atan2(y1, x1) - offsets[roinum]) % (2*math.pi)
-        e1 = find_floor_angle(angles[roinum], a1)[0]
+        e1 = closest_interval(a1, intervals[roinum])
         outp.write(','.join(map(str, [roi, idnum, e0, t0, e1, t1, warning])))
         outp.write('\n')
     outp.close()
