8d1acaf8413e509854fc3d24fbbec70b63410456
JarredAllen <jarredallen73@gmail.com>
2019-07-19 11:25:56 -0700

    Refactored bbox.py

--- scripts/bbox.py
+++ scripts/bbox.py
@@ -3,96 +3,121 @@ import numpy as np
 import cv2
 import os, os.path
 
+class BBox:
+    def __init__(self, box, poly, edges=None):
+        self._box = box
+        self._poly = poly
+        self._edges = edges
+
+    @classmethod
+    def from_str(cls, string):
+        """Creates a new BBox object from the string.
+
+        The format is "ulx,uly,width,height,angle,v1x,v1y,v2x,v2y...:e1,e2,...",
+        with the colon and everything after it being optional.
+        """
+        poly, *edges = string.split(':')
+        if edges:
+            edges = list(map(int, edges[0].split(',')))
+        else:
+            edges = None
+        poly = poly.split(',')
+        poly[:5] = list(map(float, poly[:5]))
+        box = ((poly[0], poly[1]), (poly[2], poly[3]), poly[4])
+        verts = list(zip(map(int, poly[5::2]), map(int, poly[6::2])))
+        return BBox(box, verts, edges)
+    
+    def __repr__(self):
+        """Returns a string which can be passed into the from_str method
+        to restore the original image.
+        """
+        return '%s,%s:%s' % (','.join(map(str, [self.x, self.y, self.w,
+                                                self.h, self.angle])),
+                             ','.join(map(str, sum(map(list, self.poly), []))),
+                             ','.join(map(str, self.edges)))
+    
+    def __str__(self):
+        return 'BBox:\n\tbox: {}\n\tpoly: {}\n\tedges: {}'\
+                    .format(self.box, self.poly, self.edges)
+
+    @property
+    def box(self):
+        return self._box
+    @box.setter
+    def _(self, newbox):
+        self._box = newbox
+
+    @property
+    def edges(self):
+        if self._edges is None:
+            return [i for i in range(len(self._poly))]
+        else:
+            return self._edges
+    @edges.setter
+    def _(self, newedges):
+        self._edges = newedges
+
+    @property
+    def x(self):
+        return self._box[0][0]
+
+    @property
+    def y(self):
+        return self._box[0][1]
+
+    @property
+    def w(self):
+        return self._box[1][0]
+
+    @property
+    def h(self):
+        return self._box[1][1]
+
+    @property
+    def a(self):
+        return self._box[2]
+
+    @property
+    def center(self):
+        return (int(self.x+0.5*self.w*cos(self.a)-0.5*self.h*sin(self.a),
+                int(self.y+0.5*self.h*cos(self.a)+0.5*self.w*sin(self.a))))
+
+    @property
+    def poly_abspos(self):
+        """Returns the positions of the vertices relative to the
+        original video.
+        """
+        return self._poly
+
+    @property
+    def poly_relpos(self):
+        """Returns the positions of the vertices relative to the
+        bounding box defined.
+        """
+        def transform(xy):
+            x = xy[0]-self.x
+            y = xy[1]-self.y
+            return (round(x*cos(self.a)+y*sin(self.a), 2),
+                    round(-x*sin(self.a)+y*cos(self.a), 2))
+        return list(map(transform, self.poly_abspos))
+    
+    @property
+    def box_vertices(self):
+        """Returns the vertices of the bounding rectangle, in order
+        counter-clockwise from the upper-left one.
+        """
+        ulc = np.array([self.x, self.y])
+        width = self.w*np.array([cos(self.a), sin(self.a)])
+        height = self.h*np.array([sin(self.a), cos(self.a)])
+        return [ulc, ulc+height, ulc+width+height, ulc+width]
+
 def read_bboxes(filename):
-    """Load file and returns a list of the bounding boxes inside.
-
-    Each bounding box is stored as ((upper_left_x, upper_left_y),
-    (width, height), angle, *other), where angle is between 0 and π/2
-    (radians), and *other lists the other items found in the bbox entry.
+    """Loads the given file and returns a list of BBox objects for the
+    ROIs defined in the file.
     """
-    boxes = []
-    line = open(filename).read().strip().split()
-    for i in range(len(line)):
-        box = list(map(float, line[i].split(',')))
-        boxes.append(((box[0], box[1]), (box[2], box[3]), box[4])+tuple(box[5:]))
-        # file format: [ulx,uly,width,height,angle[,...]* ]+
-    return boxes
-
-def get_center(box):
-    """Takes a bounding box (formatted as ((upper_left_x, upper_left_y),
-    (width, height), angle) and returns the x,y coordinates of the
-    center.
-    """
-    x, y = box[0]
-    x += 0.5*(box[1][0]*cos(box[2]) - box[1][1]*sin(box[2]))
-    y += 0.5*(box[1][1]*cos(box[2]) + box[1][0]*sin(box[2]))
-    return int(x), int(y)
-
-def get_box_vertices(box):
-    """Takes a bounding box (formatted as ((upper_left_x, upper_left_y),
-    (width, height), angle) and returns a list containing the four
-    vertices of the box.
-    """
-    ulc = np.array(box[0])
-    width = box[1][0]*np.array((cos(box[2]), sin(box[2])))
-    height = box[1][1]*np.array((-sin(box[2]), cos(box[2])))
-    return [ulc, ulc+width, ulc+width+height, ulc+height]
-
-def get_poly_abspos(box):
-    """Takes a bounding box and returns its vertices, formatted as a
-    list of x,y pairs, relative to the original video.
-    """
-    return list(zip(map(int, box[3::2]), map(int, box[4::2])))
-
-def get_poly_relpos(box):
-    """Takes a bounding box and returns its vertices, formatted as a
-    list of x,y pairs relative to the cropped box.
-    """
-    abspos = get_poly_abspos(box)
-    def transform(xy):
-        x = xy[0]-box[0][0]
-        y = xy[1]-box[0][1]
-        return (round(x*cos(box[2])+y*sin(box[2]), 2),
-                round(-x*sin(box[2])+y*cos(box[2]), 2))
-    return list(map(transform, abspos))
-
-def convert_polygon_to_roi(poly, padding):
-    """Takes a polygon and outputs it in ROI format. This consists of a
-    list in which the first item is a tuple with the x,y coordinates of
-    the upper-left corner of the bounding rectangle, the second is a
-    tuple specifying its length and width, and the third is its angle to
-    the vertical in radians, between 0 and π/2, measured in radians. The
-    fourth item is the polygon which was passed in.
-
-    If padding is specified, then it indicates an extra number of pixels
-    to have around all sides of the ROI.
-    """
-    (x, y), (w, h), angle = cv2.minAreaRect(poly)
-    angle = pi/180 * (90+angle)  # Convert to quadrant 1 radians
-    w, h = h, w  # swap width and height because angle is changed
-    # pad the image
-    w += 2*padding
-    h += 2*padding
-    # Shift x,y from the center (given by cv2) to the corner
-    x += -cos(angle)*w/2 + sin(angle)*h/2
-    y += -sin(angle)*w/2 - cos(angle)*h/2
-    # Round the values to the hundredths so they're easier to look at.
-    x, y, w, h, angle = map(lambda x: round(x, 2), [x, y, w, h, angle])
-    return [(x, y), (w, h), angle, poly]
-
-def flatten(lst):
-    """Flattens a list by taking any iterable element of the list and
-    expanding it to be a list of its own.
-    """
-    out = []
-    for item in lst:
-        try:
-            iter(item)
-            out += flatten(item)
-        except TypeError:
-            out.append(item)
-    return out
-
+    txt = open(filename).read().strip().split()
+    return list(map(txt, BBox.from_str))
+        
 def save_rois(rois, outfile):
     if not os.path.isdir(os.path.dirname(os.path.abspath(outfile))):
         os.makedirs(os.path.dirname(os.path.abspath(outfile)))
