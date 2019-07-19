ea3a27d5d610fcca1dac9d3e92582910896047da
JarredAllen <jarredallen73@gmail.com>
2019-07-19 15:31:46 -0700

    Added edge specifying into roimodify

--- scripts/roimodify.py
+++ scripts/roimodify.py
@@ -4,12 +4,12 @@ import numpy as np
 
 import bbox
 
-def create_boxes(boxes, newboxes):
+def create_boxes(boxes, args):
     """Takes a list of polygons of the form "x,y:x,y:..." as a string
     and turns it into a list of ROIs, formatted in the standard format
     (see bbox.py for the explanation of that format).
     """
-    for box in newboxes:
+    for box in args.create:
         boxes.append(bbox.BBox.from_verts(
             np.array(list(map(lambda x: [int(i) for i in x.split(',')],
                               box.split(':')))).reshape((-1, 1, 2)),
@@ -17,19 +17,26 @@ def create_boxes(boxes, newboxes):
         ))
     return boxes
 
-def merge_boxes(boxes, files):
+def merge_boxes(boxes, args):
     """Takes a list of files and loads all of the ROIs from them and
     adds all of them to boxes.
     """
-    for f in files:
+    for f in args.merge:
         boxes.extend(bbox.read_bboxes(f))
     return boxes
 
-def delete_boxes(boxes, nums):
+def delete_boxes(boxes, args):
     """Takes a list of boxes and returns a new list of boxes which has
     removed the boxes at indices specified by nums.
     """
-    return [boxes[i] for i in range(len(boxes)) if i not in nums]
+    return [boxes[i] for i in range(len(boxes)) if i not in args.delete]
+
+def important_edges(boxes, args):
+    """Updates boxes by defining important edges."""
+    for term in args.edges:
+        roinum, edgelist = term.split(':')
+        boxes[int(roinum)].edges = list(map(int, edgelist.split(',')))
+    return boxes
 
 def main():
     arg_parser = argparse.ArgumentParser()
@@ -38,27 +45,34 @@ def main():
                             help='The ROI file to modify.')
     subcommands = arg_parser.add_subparsers();
     delete = subcommands.add_parser('delete', help='Delete existing ROIs.')
-    delete.add_argument('input',
+    delete.add_argument('delete',
                         type=int, 
                         nargs='*',
                         help='The ROI(s) to delete.')
     delete.set_defaults(func=delete_boxes);
     create = subcommands.add_parser('add', help='Create new ROIs.')
-    create.add_argument('input',
+    create.add_argument('create',
                         type=str,
                         nargs='*',
                         help='Define the ROIs to add, each of the form '
                              'x,y:x,y:... over all vertices in order.')
     create.set_defaults(func=create_boxes);
     merge = subcommands.add_parser('merge', help='Merge ROIs in another file.')
-    merge.add_argument('input',
+    merge.add_argument('merge',
                        type=str,
                        nargs='*',
                        help='List the files containing ROIs to merge.')
     merge.set_defaults(func=merge_boxes);
+    edges = subcommands.add_parser('edges', help='Specify important edges.')
+    edges.add_argument('edges',
+                       type=str,
+                       nargs='*',
+                       help='List of important edges, formatted as '
+                            '"ROI_NUM:EDGE1,EDGE2,..."')
+    edges.set_defaults(func=important_edges)
     args = arg_parser.parse_args()
     boxes = bbox.read_bboxes(args.roifile)
-    boxes = args.func(boxes, args.input)
+    boxes = args.func(boxes, args)
     bbox.save_rois(boxes, args.roifile)
 
 if __name__ == '__main__':
